f7664b7386c09949d8454ca61f4805a3c8f1baf9
hannahschwen <hannahschwen@gmail.com>
2024-06-19 17:17:49 -0400

    adjusted SVG strings to look better

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function generateSVG(data) {
 
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 ${renderedShape.render()}
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text></svg>`
+<text x="150" y="120" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text></svg>`
 };
 
 
