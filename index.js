691e822ff3bd18121f84bc26c8a7c0a65dda6ffb
hannahschwen <hannahschwen@gmail.com>
2024-06-19 16:22:00 -0400

    wrote template for SVG in generateSVG function

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// let's just do some quick planning before I try and write tests. I just wanna start the functions :( But I gotta write tests first... and that is less fun to me. In the example, 11 tests were checked. That seems like a lot. I don't even think this'll require that many functions, so what the flip are they testing anyway? Imma go back and look at the testing examples from class.
-/*
-
-WHAT SHOULD HAPPEN:
-1. enter command node index.js
-2. users are prompted for stuff (will need inquirer)
-    - Enter text for logo. (Must not be more than 3 characters)
-    - Enter a text color
-    - Select a shape for the logo
-        - circle, square, triangle
-    - Enter a shape color
-3. logo.svg is generated
-
-I read up on writing svg, it seems pretty straight forward, just work from the format. This is basically like the readme challenge, except you're just making an svg instead of a readme.
-
-*/
-
 //Require packages
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -54,22 +37,14 @@ const writeFile = () => {};
 
 //Function to generate the SVG format
 function generateSVG(data) {
-    let text = `${data.text}`;
-    let textColor = `${data.textColor}`;
-    let shape = `${data.shape}`;
-    let shapeFill = `${data.fill}`;
 
-    //just try and make a triangle first
-    let renderedShape = new Triangle
+
+    // send input to set the fill color in the Shape class
     renderedShape.setColor(`${data.fill}`);
 
-    // console.log(`${renderedShape.line1}
-    // ${renderedShape.render()}`)
-
-    return `${renderedShape.line1}
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 ${renderedShape.render()}
-${renderedShape.line3}`
-
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text></svg>`
 };
 
 
